a860f290419a96aa0bf346248fdb67ed99bd5420
MaroshV <maros.vajs@gmail.com>
2022-02-25 22:18:40 +0200

    accept cookies
    added accept cookies button

---
+++ main.py
@@ -0,0 +1,19 @@
+# import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+PATH = 'c:\webdrivers\chromedriver.exe'
+driver = webdriver.Chrome(PATH)
+
+driver.get('https://www.nli.org.il/')
+time.sleep(10)
+element = driver.find_element(By.TAG_NAME, "button")
+element.click()
+
+# (By.CSS_SELECTOR("button.understood font-weight-bold"))
+# onclick="AddAcceptUseCookies()"
+# (SELECTOR("input[type='button']")).click()
+# driver.findElement(By.cssSelector("input[type='button']")).click();
+
+#tabindex
