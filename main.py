492bd95b7b7cdf79568d6c9ddd2e76129dc2e70c
MaroshV <maros.vajs@gmail.com>
2022-03-07 03:06:46 +0200

    Accept cookies
    Accept cookies

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-# import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 PATH = 'c:\webdrivers\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://www.nli.org.il/')
-time.sleep(3)
-element = driver.find_element(By.TAG_NAME, "button")
-element.click()
+
+try:
+    accept_cookies = WebDriverWait(driver,5).until(
+        EC.presence_of_element_located((By.TAG_NAME, "button"))
+    )
+    accept_cookies.click()
+except TimeoutException:
+    print('No accept cookies')
+
+
+
 
 # (By.CSS_SELECTOR("button.understood font-weight-bold"))
 # onclick="AddAcceptUseCookies()"
