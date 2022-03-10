c0b3b3f504a41d4386a497a14ff20dfa2134223a
MaroshV <maros.vajs@gmail.com>
2022-03-10 19:53:43 +0200

    Delete main2.py

--- main2.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-#PATH = 'c:/webdrivers/chromedriver.exe'
-#driver = webdriver.Chrome('PATH')
-driver = webdriver.Chrome(executable_path=r'C:/webdrivers/chromedriver.exe')
-driver.get('https://www.pravda.sk')
