5eb3dee93e4580cd5968c1163123b0635a555bb6
MaroshV <maros.vajs@gmail.com>
2022-03-11 00:25:33 +0200

    Search field

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#PATH = 'c:/webdrivers/chromedriver.exe'
-#driver = webdriver.Chrome('PATH')
 driver = webdriver.Chrome(executable_path=r'C:/webdrivers/chromedriver.exe')
 driver.get('https://www.nli.org.il/')
+driver.maximize_window()
 
 try:
     accept_cookies = WebDriverWait(driver, 10).until(
@@ -23,20 +22,24 @@ user_button = driver.find_element(By.ID, 'userBtn')
 user_button.click()
 login = driver.find_element(By.XPATH, '//*[@id="ulUser"]/li[1]/a').click()
 
-# user_button = driver.find_element(By.XPATH, '//*[@id="userBtn"]/div/svg').click()
-# login = driver.find_element(By.XPATH, '//*[@id="ulUser"]/li[1]/a').click()
-
+driver.implicitly_wait(30)
 username = driver.find_element(By.ID, 'loginusername')
 username.clear()
-username.send_keys('QATest1')
+username.send_keys('modonrtest@gmail.com')
 username.send_keys(Keys.RETURN)
 
 password = driver.find_element(By.ID, 'loginpassword')
 password.clear()
-password.send_keys('123456')
+password.send_keys('Test2022')
 password.send_keys(Keys.RETURN)
 
 login_button = driver.find_element(By.CLASS_NAME, 'btn _btn blue fnt dologin-btn').click()
+
+search_button = driver.find_element(By.CLASS_NAME, 'expended_search_btn js-trigger_search_btn').click()
+search_query = driver.find_element(By.ID, 'searchQuery')
+search_guery.clear()
+search_query.send_keys('Atlit')
+search_query.send_keys(Keys.RETURN)
 
 #                              *** IN PROGRESS ***
 
